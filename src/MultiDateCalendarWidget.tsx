738ffb454696af46128d4f62acdf24baa99694e5
Javier <jagiweb@gmail.com>
2023-03-02 17:58:05 +0100

    clean up of unnecessary code, added new tsx file to show in a different way those selected days. Added multiple different styling to the widget

--- src/MultiDateCalendarWidget.tsx
+++ src/MultiDateCalendarWidget.tsx
@@ -17,18 +17,19 @@ import "./ui/MultiDateCalendarWidget.css";
 import Legend from "./components/Legend";
 import { MultiDateCalendarWidgetContainerProps } from "../typings/MultiDateCalendarWidgetProps";
 import { Alert } from "./components/Alert";
+import CustomInput from "./components/CustomInput";
 
 
 
 export default function MultiDateCalendarWidget(props: MultiDateCalendarWidgetContainerProps){
-    const {lastDay, startingDay, daysArray, numberOfMonths, multiSelection, weekendDisabled, selectedColor, bgColor, reservedDates, day, mobileVersion, closeDay} = props
+    const {lastDay, startingDay, daysArray, numberOfMonths, multiSelection, weekendDisabled, selectedColor, bgColor, reservedDates, mobileVersion, closeDay} = props
     const validationFeedback = props.daysArray.validation;
     let firstSelectableDate = new Date()
     let lastestDay = 0
     let classString:string = ''
     firstSelectableDate.setDate(firstSelectableDate.getDate() + startingDay)
     
-    const [value, setValue] = useState([firstSelectableDate]);
+    const [value, setValue] = useState([]);
     const [bookedDates, setBookedDates] = useState([]);
     const [closedDates, setClosedDates] = useState([]);
 
@@ -52,55 +53,59 @@ export default function MultiDateCalendarWidget(props: MultiDateCalendarWidgetCo
         daysArray.setValue(convertDateToJson(value));
       }
     }, [value])
-
-    // useEffect(() => {
-    //   if(reservedDates.status === "available") {
-    //     console.log(reservedDates)
-    //   }
-    // }, [reservedDates])
-    
-    // if(numberOfDays.status === "available") {
-    //   numberOfDays.setValue(Big(value.length));
-    // }
-  
+    ////////////////   Booked days list /////////////
     useEffect(() =>{
     if(reservedDates?.status === "available"){
-      let requestedList:any = []
-      // let closedDaysListclosedDaysList:any = []
-      console.log('requested days ' reservedDates.value)
-      console.log('close days ' + closeDay?.value)
-      // reservedDates.items && reservedDates?.items.map( item => {
-      //   if(day && item && closeDay !== undefined){
-      //     let dayValue:any = day.get(item).value;
-      //     let closeValue:any = closeDay.get(item).value;
-          
-      //     let dayNewFormat = moment(dayValue, 'MM DD YYYY').format('MM DD YYYY')
-      //     dayValue = new DateObject(new Date(Date.parse(dayNewFormat)))
-      //     if (closeValue){
-      //       closedDates.push(`${dayValue.day}/${dayValue.month}/${dayValue.year}`)
-      //     }else{
-      //       requestedList.push(dayValue)
-      //       bookedDates.push(`${dayValue.day}/${dayValue.month}/${dayValue.year}`)
-      //     }
-          
-      //   }
-      // })
+      // creating an array and reseting the value of bookedDates
+      let listOfBookedDays = []
+      setBookedDates(listOfBookedDays)
+      if (reservedDates.value !== undefined){
+        let requestedList:any =  reservedDates.value.replace("[", '').replace(']','')
+        // As we are receiving an string of jsons inside of an array, if the value is empty we will still see "[]", in that case we replace it and check if the string is empty or not.
+        if (requestedList !== ""){
+          // if the string is not empty then we convert it into an array of string of "JSON's"
+          requestedList = requestedList.split(',')
+          requestedList.map( item => {
+              // As each item is an string of a JSON, we must convert it into an actual JSON or Object
+              let dayValue:any = JSON.parse(item);
+              dayValue = dayValue.date
+              // the format of the date is not a valid format for javascript, library "moment" helps to convert from one format to another
+              let dayNewFormat = moment(dayValue, 'DD/MM/YYYY').format('MM DD YYYY')
+              // Here we make sure that the value can be converted into a real date.
+              dayValue = new DateObject(new Date(Date.parse(dayNewFormat)))
+              listOfBookedDays.push(`${dayValue.day}/${dayValue.month}/${dayValue.year}`)
+          })
+        }
+        
+      }
     }}, [reservedDates])
-
-    // useEffect(() =>{
-    // if(closeDates?.status === "available"){
-    //   let array:any = []
-    //   closeDates.items && closeDates?.items.map( item => {
-    //     if(closeDay && item !== undefined){
-    //       let dayValue:any = day.get(item).value;
-    //       dayValue = dayValue.replace("," , "")
-    //       let dayNewFormat = moment(dayValue, 'DD/MM/YYYY').format('MM DD YYYY')
-    //       dayValue = new DateObject(new Date(Date.parse(dayNewFormat)))
-    //       array.push(dayValue)
-    //       closedDates.push(`${dayValue.day}/${dayValue.month}/${dayValue.year}`)
-    //     }
-    //   })
-    // }}, [closeDates])
+    ////////////////   Close days list /////////////
+    useEffect(() =>{
+      if(closeDay?.status === "available"){
+        // creating an array and reseting the value of closedDays
+        let listOfClosedDays = []
+        setClosedDates(listOfClosedDays)
+        if (closeDay.value !== undefined){
+          let closedDaysListofJson:any =  closeDay.value.replace("[", '').replace(']','')
+          // As we are receiving an string of jsons inside of an array, if the value is empty we will still see "[]", in that case we replace it and check if the string is empty or not.
+          if (closedDaysListofJson !== ""){
+            // if the string is not empty then we convert it into an array of string of "JSON's"
+            closedDaysListofJson = closedDaysListofJson.split(',')
+            closedDaysListofJson.map( item => {
+              // As each item is an string of a JSON, we must convert it into an actual JSON or Object
+                let dayValue:any = JSON.parse(item);
+                dayValue = dayValue.date
+                // the format of the date is not a valid format for javascript, library "moment" helps to convert from one format to another
+                let dayNewFormat = moment(dayValue, 'DD/MM/YYYY').format('MM DD YYYY')
+                // Here we make sure that the value can be converted into a real date.
+                dayValue = new DateObject(new Date(Date.parse(dayNewFormat)))
+                // we push the dates as an string so we can compare this dates with the date of the calendar
+                listOfClosedDays.push(`${dayValue.day}/${dayValue.month}/${dayValue.year}`)
+            })
+          }
+          
+        }
+      }}, [closeDay])
 
     if (mobileVersion){
       classString = `${selectedColor} bg-${bgColor} rmdp-mobile`
@@ -112,6 +117,7 @@ export default function MultiDateCalendarWidget(props: MultiDateCalendarWidgetCo
         <Fragment>
   
           <DatePicker 
+          render={<CustomInput />}
           weekStartDayIndex={1}
           value={value} 
           onChange={setValue} 
@@ -120,7 +126,6 @@ export default function MultiDateCalendarWidget(props: MultiDateCalendarWidgetCo
           minDate={firstSelectableDate}
           maxDate={lastestDay}
           numberOfMonths={numberOfMonths}         
-          // formatWeekDay={(day: string) => day.substring(0,3)}
           mapDays={({ date }) => { 
             let calendarDate = `${date.day}/${date.month}/${date.year}`
             let booked = bookedDates.includes(calendarDate)
@@ -133,15 +138,17 @@ export default function MultiDateCalendarWidget(props: MultiDateCalendarWidgetCo
                 style: { color: "#7d7c7c" }
               }
             }
-
+            // bookedDays list represented in the calendar as the style below
             if (booked) return {
               disabled: true, 
-              style: { color: "green", fontWeight: "bold" },
+              title: "Day already booked",
+              class: "booked-days"
             }
-
+            // closedDays list represented in the calendar as the style below
             if (closed) return {
               disabled: true, 
-              style: { color: "red", fontWeight: "bold"  },
+              title: "Office is closed",
+              class: "office-closed"
             }
 
 
@@ -151,7 +158,10 @@ export default function MultiDateCalendarWidget(props: MultiDateCalendarWidgetCo
           
           
           plugins={[
-            <DatePanel />,            
+            <DatePanel 
+              className="black-remove"
+              sort="date"
+            />,            
             // <DatePickerHeader 
             // position="top" 
             // size="small" />,
---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,24 @@
+import {createElement, Fragment} from 'react'
+import '../ui/MultiDateCalendarWidget.css'
+
+function CustomInput(props:any) {
+    const {openCalendar, value} = props
+
+  return (
+    <Fragment>
+        <div>
+        <input className='input-margin' readOnly value='Click to select dates' onFocus={openCalendar}/>
+        </div>
+        
+        
+        <div className='daylist-container'>
+            {value.map(date => {
+                return <span className='selected-date-cl'>{date}</span>
+            })}
+        </div>
+    </Fragment>
+    
+  )
+}
+
+export default CustomInput
